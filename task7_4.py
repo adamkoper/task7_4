b3a95e357e29e9fce59ff96e000cd0048ae18def
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-09 21:50:09 +0200

    add play

--- task7_4.py
+++ task7_4.py
@@ -5,6 +5,12 @@ class Films:
         self.type = type
         self.nr_view = nr_view
 
+        # Variables
+        self.nr_plays = 0
+
+    def play(self, step=1):
+        self.nr_plays += step
+
 
 class Series(Films):
     def __init__(self, nr_episode, nr_season, *args, **kwargs):
