270b07d025cf322766ec74236a20984eb709c230
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-09 21:51:07 +0200

    add def __str__ and __repr__

--- task7_4.py
+++ task7_4.py
@@ -11,12 +11,27 @@ class Films:
     def play(self, step=1):
         self.nr_plays += step
 
+    def __str__(self):
+        return f'{self.title} {self.year}'
+
+    def __repr__(self):
+        return f"Films(title={self.title} year={self.year}, type={self.type}, view={self.nr_view})"
+
 
 class Series(Films):
     def __init__(self, nr_episode, nr_season, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.nr_episode = nr_episode
         self.nr_season = nr_season
+
+    def __str__(self):
+        return f'{self.title} S{self.nr_season}E{self.nr_episode}'
+
+    def __repr__(self):
+        return f"Series(title={self.title} year={self.year}, type={self.type}, nr_view={self.nr_view}, nr_episode={self.nr_episode}," \
+               f"nr_season={self.nr_season})"
+
+
 
 
 
